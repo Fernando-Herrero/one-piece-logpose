e277017b8b00a1558e09de58bfe08f0915b5d4f7
Fernando Herrero <herrerofernando.94@gmail.com>
2025-12-10 11:51:54 +0100

    feature(main): add ErrorBondary to main

--- src/main.jsx
+++ src/main.jsx
@@ -1,3 +1,5 @@
+import { ErrorBoundary } from "@/components/ErrorBoundary/ErrorBoundary.jsx";
+import { PageError } from "@/components/ErrorBoundary/PageError.jsx";
 import { NotificationsProvider } from "@/context/NotificationsContext.jsx";
 import { NotificationsCountProvider } from "@/context/NotificationsCountContext.jsx";
 import { PostProvider } from "@/context/PostContext.jsx";
@@ -14,27 +16,39 @@ import { SagaProvider } from "./context/SagaContext.jsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-    <BrowserRouter>
-        <LanguagesProvider>
-            <AuthProvider>
-                <AvatarProvider>
-                    <PostProvider>
-                        <UsersProvider>
-                            <NotificationsCountProvider>
-                                <NotificationsProvider>
-                                    <SagaProvider>
-                                        <ModdalProvider>
-                                            <DeviceProvider>
-                                                <App />
-                                            </DeviceProvider>
-                                        </ModdalProvider>
-                                    </SagaProvider>
-                                </NotificationsProvider>
-                            </NotificationsCountProvider>
-                        </UsersProvider>
-                    </PostProvider>
-                </AvatarProvider>
-            </AuthProvider>
-        </LanguagesProvider>
-    </BrowserRouter>
+    <ErrorBoundary
+        fallback={
+            <PageError
+                title="Ups… algo no funcionó"
+                message="Esto no debería haber pasado…Por favor, recarga la página para continuar."
+                onRetry={() => window.location.reload()}
+                retryText="Recargar página"
+                fullPage
+            />
+        }
+    >
+        <BrowserRouter>
+            <LanguagesProvider>
+                <AuthProvider>
+                    <AvatarProvider>
+                        <PostProvider>
+                            <UsersProvider>
+                                <NotificationsCountProvider>
+                                    <NotificationsProvider>
+                                        <SagaProvider>
+                                            <ModdalProvider>
+                                                <DeviceProvider>
+                                                    <App />
+                                                </DeviceProvider>
+                                            </ModdalProvider>
+                                        </SagaProvider>
+                                    </NotificationsProvider>
+                                </NotificationsCountProvider>
+                            </UsersProvider>
+                        </PostProvider>
+                    </AvatarProvider>
+                </AuthProvider>
+            </LanguagesProvider>
+        </BrowserRouter>
+    </ErrorBoundary>
 );
