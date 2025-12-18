25bff73b579bf892b22e0027c5d30dd33d903388
Fernando Herrero <herrerofernando.94@gmail.com>
2025-12-18 18:07:21 +0100

    refactor(Purchases): add trnaslate Purchases and all components inside

--- src/dashboard/pages/Purchases.jsx
+++ src/dashboard/pages/Purchases.jsx
@@ -1,33 +1,29 @@
 import { AuthContext } from "@/context/AuthContext";
-import { LanguagesContext } from "@/context/LanguagesContext";
-import { languages } from "@/helpers/languages";
+import { useTranslate } from "@/translations/useTranslate";
 import { useContext } from "react";
 
 const Purchases = () => {
     const { isVerified } = useContext(AuthContext);
-    const { lang } = useContext(LanguagesContext);
+    const { t } = useTranslate();
 
     return (
         <section className="flex flex-col items-center justify-center min-h-screen p-6">
             <div className="max-w-md w-full bg-sunny rounded-2xl shadow p-8 text-center">
-                <h2 className="text-2xl font-semibold mb-4 text-primary">
-                    🛒 {languages[lang].purchases.purchases}
-                </h2>
+                <h2 className="text-2xl font-semibold mb-4 text-primary">🛒 {t("purchases.purchases")}</h2>
 
                 {isVerified ? (
                     <div className="flex flex-col items-center gap-2 text-green-600">
                         <span className="text-4xl">✅</span>
-                        <p className="font-medium">{languages[lang].purchases.verified}</p>
-                        <p className="text-sm text-gray-600">{languages[lang].purchases.thanks}</p>
+                        <p className="font-medium">{t("purchases.verified")}</p>
+                        <p className="text-sm text-gray-600">{t("purchases.thanks")}</p>
                     </div>
                 ) : (
                     <div className="flex flex-col items-center gap-2 text-muted">
                         <span className="text-4xl">⚪</span>
-                        <p className="font-medium">{languages[lang].purchases.noPurchases}</p>
+                        <p className="font-medium">{t("purchases.no_purchases")}</p>
                         <p className="text-sm text-muted">
-                            {languages[lang].purchases.goTo}{" "}
-                            <strong>{languages[lang].purchases.settingPremium}</strong>{" "}
-                            {languages[lang].purchases.verifyAccount}
+                            {t("purchases.go_to")} <strong>{t("purchases.setting_premium")}</strong>{" "}
+                            {t("purchases.verify_account")}
                         </p>
                     </div>
                 )}
