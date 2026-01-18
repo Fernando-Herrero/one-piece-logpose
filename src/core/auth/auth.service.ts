006c507b3f66a0aa0ca612fe449907dc330db1f9
Fernando Herrero <herrerofernando.94@gmail.com>
2026-01-18 14:00:39 +0100

    feat(auth.service): add types to auth.service

--- src/core/auth/auth.service.ts
+++ src/core/auth/auth.service.ts
@@ -1,13 +1,14 @@
 import { storage } from "../../helpers/storage";
+import type { User } from "../../types/auth.types";
 
-export const saveUserInLocalStorage = (user) => storage.save("user", user);
+export const saveUserInLocalStorage = (user: User) => storage.save("user", user);
 
-export const getUserFromLocalStorage = () => storage.get("user");
+export const getUserFromLocalStorage = (): User | null => storage.get("user");
 
 export const removeUserFromLocalStorage = () => storage.remove("user");
 
-export const saveTokenInLocalStorage = (token) => storage.save("token", token);
+export const saveTokenInLocalStorage = (token: string) => storage.save("token", token);
 
-export const getTokenFromLocalStorage = () => storage.get("token");
+export const getTokenFromLocalStorage = (): string | null => storage.get("token");
 
 export const removeTokenFromLocalStorage = () => storage.remove("token");
