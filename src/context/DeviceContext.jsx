e7b75f5b5b0c3ccafe19017a611224018694fa13
Fernando Herrero <herrerofernando.94@gmail.com>
2026-02-12 13:03:32 +0100

    refactor(DeviceContext): refactor context to use breakpoint instead width

--- src/context/DeviceContext.jsx
+++ src/context/DeviceContext.jsx
@@ -2,18 +2,29 @@ import { createContext, useEffect, useMemo, useState } from "react";
 
 export const DeviceContext = createContext(null);
 
+const getDeviceFromWidth = (w) => {
+    if (w >= 1280) return "desktopXl";
+    if (w >= 1024) return "desktop";
+    if (w >= 768) return "tabletXl";
+    if (w >= 550) return "tablet";
+    if (w >= 376) return "mobile";
+    return "mobileXs";
+};
+
 export const DeviceProvider = ({ children }) => {
-    const [width, setWidth] = useState(window.innerWidth);
+    const [device, setDevice] = useState(getDeviceFromWidth(window.innerWidth));
 
     useEffect(() => {
-        const updateWidth = () => setWidth(window.innerWidth);
+        const updateDevice = () => {
+            const newDevice = getDeviceFromWidth(window.innerWidth);
+            setDevice((prev) => (prev !== newDevice ? newDevice : prev));
+        };
 
-        window.addEventListener("resize", updateWidth);
-
+        window.addEventListener("resize", updateDevice);
         return () => window.removeEventListener("resize", updateWidth);
     }, []);
 
-    const value = useMemo(() => ({ width }), [width]);
+    const value = useMemo(() => ({ device }), [device]);
 
     return <DeviceContext.Provider value={value}>{children}</DeviceContext.Provider>;
 };
